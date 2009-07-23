022aeb5645505ff7920c8720290b53b9ddc6d395
David Brady <dbrady@shinybit.com>
2009-07-23 11:45:16 -0600

    * Extracted aliases into separate file. * Refactored dotprofile, wrote a bash function 'include_if_exists' * dotprofile now includes dotaliases if it exists * setup.sh links .aliases -> ~/bin/dotaliases

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # This should be run from ~/.dotfiles. YOU fix the hardcodes if you don't like 'em.
 cd
 ln -s ~/.dotfiles/bash_completions .bash_completions
+ln -s ~/.dotfiles/dotaliases.el .aliases
 ln -s ~/.dotfiles/dotemacs.el .emacs
 ln -s ~/.dotfiles/dotirbrc .irbrc
 ln -s ~/.dotfiles/dotprofile .profile
