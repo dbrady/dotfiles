65f335a680d133a9330c9e4a778847972efa7c8e
David Brady <dbrady@shinybit.com>
2013-08-29 12:08:57 -0600

    move {,.}dotfiles folder in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-# This should be run from ~/.dotfiles. YOU fix the hardcodes if you don't like 'em.
-cd
-ln -s ~/.dotfiles/bash_completions .bash_completions
-ln -s ~/.dotfiles/dotaliases .aliases
-ln -s ~/.dotfiles/dotemacs.el .emacs
-ln -s ~/.dotfiles/dotvimrc .vimrc
-ln -s ~/.dotfiles/dotirbrc .irbrc
-ln -s ~/.dotfiles/dotprofile .profile
-ln -s ~/.dotfiles/git-completion.bash .git-completion.bash
-ln -s ~/.dotfiles/dotprivate .private
-ln -s ~/.dotfiles/dotnav .nav
-cd ~/.dotfiles
+cd ~
+ln -s ~/dotfiles/bash_completions .bash_completions
+ln -s ~/dotfiles/dotaliases .aliases
+ln -s ~/dotfiles/dotemacs.el .emacs
+ln -s ~/dotfiles/dotvimrc .vimrc
+ln -s ~/dotfiles/dotirbrc .irbrc
+ln -s ~/dotfiles/git-completion.bash .git-completion.bash
+ln -s ~/dotfiles/dotprivate .private
+ln -s ~/dotfiles/dotnav .nav
