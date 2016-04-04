87ecfec987fde4226121909b371f06a445a4b610
David Brady <dbrady@shinybit.com>
2016-04-04 12:25:05 -0600

    Extract json_load_file.rb

---
+++ json_load_file.rb
@@ -0,0 +1,12 @@
+# loading a file from disk that contains JSON directly into memory is a huge
+# PITA in Ruby, and it shouldn't be. This creates a method directly on the
+# module, JSON.load_file(filename), that does exactly what you'd expect.
+require 'json'
+module JSON
+  def self.load_file(filename)
+    File.open(filename) do |io|
+      load io
+    end
+  end
+end
+$stderr.puts "So, doin' a lot of JSON work lately, eh? I just created JSON.load_file(filename) for you. You're welcome."
