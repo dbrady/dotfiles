d616b3eff4554420346383d86971238cb0fec66c
David Brady <dbrady@covermymeds.com>
2017-09-25 13:50:59 -0600

    Add if test version (bash script example)

--- osx_or_linux.sh
+++ osx_or_linux.sh
@@ -5,7 +5,12 @@ case `uname -a | awk '{print $1}'` in
     Linux)
         echo 'This is running on Linux!' ;;
     Darwin)
-        echo 'This is runnning on OSX!' ;;
+        echo 'This is running on OSX!' ;;
     *)
         echo 'I have no clue what OS this is. :-(' ;;
 esac
+
+
+# Here's the same thing as an if test statement
+if [ `uname -a | awk '{print $1}'` = 'Linux' ]; then echo 'This is running on Linux!'; fi
+if [ `uname -a | awk '{print $1}'` = 'Darwin' ]; then echo 'This is running on OSX!'; fi
